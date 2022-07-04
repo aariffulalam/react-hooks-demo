cb7ba0353c01dc9accb13196c55d2bfc1732dc48
aariffulalam <aarif20@navgurukul.org>
2022-07-04 14:59:49 +0530

    otimized useEffect Hook by specifying the second conditional perameter.

--- src/component/useEffect/EffectHookCouterOne.jsx
+++ src/component/useEffect/EffectHookCouterOne.jsx
@@ -2,14 +2,16 @@ import React, {useState, useEffect} from 'react'
 
 const EffectHookCouterOne = () => {
   const [count, setCount] = useState(0)
+  const [name, setName] = useState("")
   useEffect(()=>{
+    console.log("count edited")
     document.title =`You clicked ${count} times`
-  })
+  },[count])
   return (
     <>
       <h3>EffectHookCouterOne</h3>
-      <button onClick={()=>setCount(pre => pre+1)}>increment</button>
-      <h4>{count}</h4>
+      <input type="text" vlaue={name} onChange={(e)=> setName(e.target.value)} />
+      <button onClick={()=>setCount(pre => pre+1)}>increment {count}</button>
     </>
   )
 }
